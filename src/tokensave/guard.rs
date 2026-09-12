a99247cbd3af3239db4f5f36b62d294ddc53cfd6
rNoz <rnoz.commits@gmail.com>
2026-09-12 19:55:51 +0200

    test(branch): allow unwraps in lock tests

--- src/tokensave/guard.rs
+++ src/tokensave/guard.rs
@@ -276,6 +276,7 @@ pub(crate) fn is_pid_alive(pid: u32) -> bool {
 }
 
 #[cfg(test)]
+#[allow(clippy::unwrap_used, clippy::expect_used)]
 mod tests {
     use super::*;
     use std::time::Duration;
